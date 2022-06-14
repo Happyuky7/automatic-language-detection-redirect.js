f3c9825655c5aded03a59a081b3da9915248a30b
HappyRogelio7 <58347721+HappyRogelio7@users.noreply.github.com>
2022-06-14 19:28:46 -0400

    Add files via upload

---
+++ automatic-language-detection-redirect.js
@@ -0,0 +1,11 @@
+//original code: https://gist.github.com/fv0/f8acf201faee95cf02e9365dcd0d46a0
+
+// Set of supported languages
+var supportedLanguages = { en: "en", en: "en-US", es: "es", es: "es-ES", es: "es-MX", es: "es-CL", jp: "jp", fr: "fr", de: "de", it: "it", pt: "pt", ru: "ru", zh: "zh", tr: "tr" };
+var fallbackLanguage = "en";
+
+// Check if the users browser language is a supported language of the website. Otherwise go to fallback.
+var language = supportedLanguages[(navigator.language || navigator.userLanguage).substring(0,2)] || fallbackLanguage;
+
+// Go to the language version of your website. Example: https://domain.com/en/
+location.replace("." +  "/" + language + "/" + "index.html");
